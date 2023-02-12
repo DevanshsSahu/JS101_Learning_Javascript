07ec6e612341b0f2256d562b14329c542f9e7a84
unknown <devanshsahuyanim@gmail.com>
2023-02-12 19:07:29 +0530

    second commit

---
+++ que1.js
@@ -0,0 +1,7 @@
+let a=33
+if(a%3==0){
+  console.log("A multiple of 3")
+}
+else{
+  console.log('not a multiple')
+}
---
+++ que4.js
@@ -0,0 +1,11 @@
+let username='hinata;)' , password='106'
+let input_username='hinata;)', input_password='106'
+
+if(username==input_username){
+  if(password==input_password){
+    console.log('you can login')
+  }
+  else{
+    console.log('incorrect username or password')
+  }
+}
